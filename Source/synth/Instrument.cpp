3926b4c55775f6d77d0d9e14ac03b7eb1710d98f
Vincent Schüßler <v.schuessler@gmail.com>
2014-10-09 11:43:11 +0200

    Fix some warnings and inaccuracies

--- Source/synth/Instrument.cpp
+++ Source/synth/Instrument.cpp
@@ -42,40 +42,36 @@ double Instrument::getEnvelope(bh_time time) const {
 	const double releaseSamples = release / 1000 * sampleRate;
 
 	const bh_time timeSinceTrigger = time - triggerTime;
-
-	float factor;
 	if (isNoteOn) {
-		if (timeSinceTrigger < attackSamples) {
-			factor = timeSinceTrigger / attackSamples;
+		if (timeSinceTrigger <= attackSamples && attackSamples > 0) {
+			return timeSinceTrigger / attackSamples;
 		}
-		else if (timeSinceTrigger - attackSamples < decaySamples) {
-			const double t = (timeSinceTrigger - attackSamples) / decaySamples;
-			factor = (1 - t) + sustain * t;
+		else if (timeSinceTrigger - attackSamples - 1 <= decaySamples && decaySamples > 0) {
+			const double t = (timeSinceTrigger - attackSamples - 1) / decaySamples;
+			return (1 - t) + sustain * t;
 		}
 		else {
-			factor = sustain;
+			return sustain;
 		}
 	}
 	else {
-		if (timeSinceTrigger < releaseSamples) {
+		if (timeSinceTrigger <= releaseSamples && releaseSamples > 0) {
 			const double t = timeSinceTrigger / releaseSamples;
-			factor = releaseLevel * (1 - t);
+			return releaseLevel * (1 - t);
 		}
 		else {
-			factor = 0.;
+			return 0.;
 		}
 	}
-
-	return factor;
 }
 
 void Instrument::play(bh_time time, int start, int samples, int channels, float* const * const buffer) const {
-	for (int i = start; i < start + samples; i++) {
+	for (int i = 0; i < samples; i++) {
 		const double TAU = 2. * std::acos(-1);
 		const bh_time currentTime = time + i;
-		const float value = std::sin(double(currentTime) / sampleRate * frequency * TAU) * getEnvelope(currentTime);
+		const float value = float(std::sin(double(currentTime) / sampleRate * frequency * TAU) * getEnvelope(currentTime));
 		for (int channel = 0; channel < channels; channel++) {
-			buffer[channel][i] = value;
+			buffer[channel][start + i] = value;
 		}
 	}
 }
